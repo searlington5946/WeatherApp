8ea7a0f276fe6740d2427250e10d5a505e44c0f9
Seth Arlington <searlington5946@students.uaptc.edu>
2018-08-27 21:32:48 -0500

    Added a compass to the weather information.

---
+++ WeatherApp/merges/android/scripts/compass.js
@@ -0,0 +1,25 @@
+﻿function getCompassDirection() {
+
+    var options = {
+        frequency: 3000
+    };
+
+    navigator.compass.watchHeading(onSuccess, onError, options);
+    navigator.geolocation.getCurrentPosition(onGeoSuccess, onGeoError);
+    
+    function onSuccess(heading) {
+        $("#degree").text(heading.magneticHeading);
+        $("#heading").text(degToCompass(heading.magneticHeading));
+    }
+
+    function onError(error) {
+        $("#degree").text(error.code);
+    }
+
+    // https://stackoverflow.com/questions/7490660/converting-wind-direction-in-angles-to-text-words#7490772
+    function degToCompass(num) {
+        var val = Math.floor((num / 22.5) + 0.5);
+        var arr = ["N", "NNE", "NE", "ENE", "E", "ESE", "SE", "SSE", "S", "SSW", "SW", "WSW", "W", "WNW", "NW", "NNW"];
+        return arr[(val % 16)];
+    }
+}
